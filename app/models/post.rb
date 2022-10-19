d1076a27aa2931c9c72360426a1caa23670d0f9a
richez003 <richemonday@gmail.com>
2022-10-19 15:55:31 -0500

    likes_counter validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
 
 validate :title, presence: true, length: { minimum: 250 }
 validate :comments_counter, numericallity: { only_integer: true,  greater_than_or_equal_to: 0 }
+validate :likes_counter, numericallity: { only_integer: true,  greater_than_or_equal_to: 0 }
 
 
   def update_post_counter
