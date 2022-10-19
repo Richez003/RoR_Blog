0267be800f9fc61c464ff6882ff6cfc458e3bfe4
richez003 <richemonday@gmail.com>
2022-10-19 15:44:52 -0500

    posts_counter validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true
- 
+  validate :posts_counter, numericallity: { only_integer: true,  greater_than_or_equal_to: 0 }
+
   private
 
   def three_recent_post
