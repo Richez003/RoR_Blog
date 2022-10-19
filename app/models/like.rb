8f0259267ad2f43a2a8a904e9ec7c4e346c3ae1d
richez003 <richemonday@gmail.com>
2022-10-18 19:27:56 -0500

    fixed rubocop errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :user,  foreign_key: 'user_id', class_name: 'Post'
+  belongs_to :user, foreign_key: 'user_id', class_name: 'Post'
   belongs_to :post
 
   def update_post_like_count
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts,  foreign_key: 'user_id', class_name: 'Post'
-  has_many :likes,  foreign_key: 'user_id', class_name: 'Like'
-  has_many :comments,  foreign_key: 'user_id', class_name: 'Comment'
+  has_many :posts, foreign_key: 'user_id', class_name: 'Post'
+  has_many :likes, foreign_key: 'user_id', class_name: 'Like'
+  has_many :comments, foreign_key: 'user_id', class_name: 'Comment'
 
-  
- private
+  private
+
   def three_recent_post
     posts.order(created_at: :desc).limit(3)
   end
