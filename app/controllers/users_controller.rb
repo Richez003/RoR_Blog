f03f64c0ed472283b8912551f029fddb0d5fd2e4
richez003 <richemonday@gmail.com>
2022-10-26 13:40:50 -0500

    wrote test for controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    # @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
-    @post = @user.posts.find(params[:id])
+    # @user = User.find(params[:id])
+    # @post = @user.posts.find(params[:id])
   end
 end
