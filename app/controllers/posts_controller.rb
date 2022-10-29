8def418396447a38cbfac3f45b729f8ad01c6183
richez003 <richemonday@gmail.com>
2022-10-29 13:17:10 -0500

    modified user views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.find(params[:user_id])
+    @posts = @user.posts
+    # @posts = Post.includes(:user, :comments, :likes).where(user_id: params[:user_id]).order(created_at: :desc)
+
     # @posts.includes([:comments])
   end
 
@@ -10,8 +12,23 @@ class PostsController < ApplicationController
     @posts = @user.posts.find(params[:user_id])
   end
 
-  def create
+  def new
     @post = Post.new
-    @post.save
   end
+  def create
+    @user = current_user
+    @post = @user.posts.new(post_params)
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+    if @post.save
+      redirect_to user_posts_path(@user.id)
+    else
+      render :new
+    end
+  end
+private
+
+def post_params
+  params.require(:post).permit(:text, :title, :user_id)
+end
 end
