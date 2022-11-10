2f3866f8e6f02a4698f22624d2a623b73543cb1d
richez003 <richemonday@gmail.com>
2022-11-10 00:45:20 -0600

    devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name bio password password_confirmation])
+    
   end
 end
